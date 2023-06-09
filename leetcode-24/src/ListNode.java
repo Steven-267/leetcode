e995b3b8455da42948f0a4b77ae1491614cc0e6e
Steven-267 <aa030726@126.com>
2023-06-09 23:44:01 +0800

    leetcode-24

---
+++ leetcode-24/src/ListNode.java
@@ -0,0 +1,24 @@
+public class ListNode {
+    private  int val;
+    ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public int getVal() {
+        return val;
+    }
+
+    public void setVal(int val) {
+        this.val = val;
+    }
+
+    public ListNode getNext() {
+        return next;
+    }
+
+    public void setNext(ListNode next) {
+        this.next = next;
+    }
+}
