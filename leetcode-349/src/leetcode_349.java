232b6750dd8686c8c4a1bcadb9abee9826c14dff
Steven-267 <aa030726@126.com>
2023-06-13 12:54:10 +0800

    leetcode-349

---
+++ leetcode-349/src/leetcode_349.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class leetcode_349 {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums1.length == 0||nums2.length == 0||nums2.length == 0){
+            return new int[0];
+        }
+        Set<Integer> set1  = new HashSet<>();
+        Set<Integer> resSet = new HashSet<>();
+
+        for (int i :
+                nums1) {
+            set1.add(i);
+        }
+        for (int i :
+                nums2) {
+            if (set1.contains(i)) {
+                resSet.add(i);
+            }
+        }
+        return resSet.stream().mapToInt(s -> s).toArray();
+
+    }
+}
